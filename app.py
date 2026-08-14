976af85c8ade9af668ad766903174147feb7638a
tkien1 <kienle122az@gmail.com>
2026-08-14 16:21:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ from io import BytesIO
 # CẤU HÌNH
 # ==========================================
 
-st.set_page_config(
+st.set_page_config(st.image("logo.jpg")
     page_title="Quản lý khách hàng",
     page_icon="👤",
     layout="wide"
 )
-st.image("logo.jpg")
+
 
 # ==========================================
 # KHỞI TẠO DANH SÁCH KHÁCH HÀNG
